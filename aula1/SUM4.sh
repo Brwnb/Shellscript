6bb92adfb612d510cfcbfcee88780d3c470afd45
Borges Borges <bruno2.0oliveira@gmail.com>
2022-06-26 11:16:03 -0300

    REVISAO3

--- aula1/SUM4.sh
+++ aula1/SUM4.sh
@@ -4,22 +4,31 @@
 # 		The user must input two value, the script
 #		will sum them and print on the screen
 
-echo 
-echo 
-echo $*
-echo $1 $2 | egrep [0-9] >> /dev/null
-if [[ $? -eq 0 ]]; then
-	sum=$(echo $1 + $2 | bc)
-	echo "The result is: " $sum 
-	echo
-	echo
 
+# 
+minimun=$(echo $* | wc -w)
+if [[ $minimun -gt 1 ]]; then
+	echo $1 $2 | egrep [0-9] >> /dev/null
+	if [[ $? -eq 0 ]]; then
+		sum=$(echo $1 + $2 | tr , . | bc)
+		echo
+		echo
+		echo "The result is: " $sum 
+		echo
+		echo
+
+	else
+		echo
+		echo
+		echo "The Values aren't numbers "
+		echo 
+		echo
+
+	fi
 else
 	echo
 	echo
-	echo "The Values aren't numbers "
-	echo 
+	echo "Insert two values in the system"
 	echo
-
+	echo
 fi
-
